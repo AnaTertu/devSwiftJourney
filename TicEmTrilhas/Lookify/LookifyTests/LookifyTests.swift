a7c3a473f601292029572be3c6414cb51df0bf46
AnaTertu <ana.tertu.2019@gmail.com>
2025-03-03 18:32:08 -0300

    criado projeto

---
+++ TicEmTrilhas/Lookify/LookifyTests/LookifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LookifyTests.swift
+//  LookifyTests
+//
+//  Created by ana on 03/03/25.
+//
+
+import Testing
+@testable import Lookify
+
+struct LookifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
