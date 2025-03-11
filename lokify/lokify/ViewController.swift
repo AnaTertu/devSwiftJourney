d8400dbe37f2a671ab90ee1e0c5ca3740b9e2d59
AnaTertu <ana.tertu.2019@gmail.com>
2025-03-11 15:52:20 -0300

    Navegação entre telas - reuso comportamento backgroudColor e integração para alteração na mudança tempo

--- lokify/lokify/ViewController.swift
+++ lokify/lokify/ViewController.swift
@@ -42,12 +42,12 @@ class ViewController: UIViewController {
     
     // a segue vai chamar essa função qdo botão for clicado
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         
+        
         if segue.identifier == "toDetailsShift" { // aqui desemcapsulamos a view controller guardando numa constante que vai receber o destino dessa segue
             let detailsShiftViewController = segue.destination as! DetailsShiftViewController // usamos force ! pq sabemos q é essa class
             detailsShiftViewController.shift = self.shift // a shift da deteils recebe a shift desta página, passamos qual é o turno de uma tela pra a outra
+            detailsShiftViewController.view.backgroundColor = self.view.backgroundColor
         }
     }
-    
 }
 
