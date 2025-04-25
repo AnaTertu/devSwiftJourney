988f0aaa7aef2a63b0450a43d3038555421b36fc
AnaTertu <ana.tertu.2019@gmail.com>
2025-04-25 08:25:36 -0300

    celulas organizadas e boleadas com cores diversas

--- collectionView/collectionView/CategoryCollectionViewCell.swift
+++ collectionView/collectionView/CategoryCollectionViewCell.swift
@@ -3,10 +3,16 @@
 //  collectionView
 //
 //  Created by ana on 24/04/25.
-//
+// 
 
 import UIKit
 
-class CategorieCollectionViewCell: UICollectionViewCell {
+class CategoryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var title: UILabel!
     
+    @IBOutlet weak var background: UIView!
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib( )
+    }
 }
