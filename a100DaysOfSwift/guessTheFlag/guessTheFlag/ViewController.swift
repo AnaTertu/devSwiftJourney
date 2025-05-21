c74e4b72773a646c6c5f5693f40bb2742e9372ae
AnaTertu <ana.tertu.2019@gmail.com>
2025-05-21 14:52:32 -0300

    assets catalog - Buttons

---
+++ a100DaysOfSwift/guessTheFlag/guessTheFlag/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  guessTheFlag
+//
+//  Created by ana on 21/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
