0519e4bfecee587d98d86d80206f5abdc1b14ad3
AnaTertu <ana.tertu.2019@gmail.com>
2025-05-21 20:07:54 -0300

    actions: creating an IBAction

--- a100DaysOfSwift/guessTheFlag/guessTheFlag/ViewController.swift
+++ a100DaysOfSwift/guessTheFlag/guessTheFlag/ViewController.swift
@@ -8,23 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
     var countries: [String] = [String]()
     var score = 0
-    
+    var correctAnswer = 0
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        countries.append(contentsOf: ["Brasil", "Estonia", "France", "Germany", "Ireland", "Italy", "Monaco",  "Nigeria", "Spain", "Uk", "Ukraine", "Usa"])
-        //countries += ["Brasil", "Estonia", "France", "Germany", "Ireland", "Italy", "Monaco",  "Nigeria", "Spain", "Uk", "Ukraine", "Usa"]
+        //countries.append(contentsOf: ["Brasil", "Estonia", "France", "Germany", "Ireland", "Italy", "Monaco",  "Nigeria", "Spain", "Uk", "Ukraine", "Usa"])
+        countries += ["Brasil", "Estonia", "France", "Germany", "Ireland", "Italy", "Monaco",  "Nigeria", "Spain", "Uk", "Ukraine", "Usa"]
         
-        button1.layer.borderWidth = 2
-        button2.layer.borderWidth = 2
-        button3.layer.borderWidth = 2
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
         
         button1.layer.borderColor = UIColor.orange.cgColor
         button2.layer.borderColor = UIColor.orange.cgColor
@@ -32,13 +32,40 @@ class ViewController: UIViewController {
         
         askQuestion()
     }
-    
-    func askQuestion() {
+        
+    func askQuestion(action: UIAlertAction? = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+        
+        
     }
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your scores is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+        
+    }
+    
 
 }
 
