8a7c077980ce8cdd2a3cc76c196522306cf2ea15
AnaTertu <ana.tertu.2019@gmail.com>
2025-05-23 14:48:27 -0300

    View website - "https://github.com/AnaTertu" First step complete!

--- a100DaysOfSwift/EasyBrowser_FourthProject/EasyBrowser_FourthProject/ViewController.swift
+++ a100DaysOfSwift/EasyBrowser_FourthProject/EasyBrowser_FourthProject/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  EasyBrowser_FourthProject
-//
-//  Created by ana on 23/05/25.
-//
-
+import WebKit
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var webView: WKWebView!
+                // visualização de carregamento
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
     }
-
+                //visualização Carregou
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://github.com/AnaTertu")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+                // Visualização Aparecerá
+    override func viewWillAppear(_ animated: Bool) {
+        /* super.viewWillAppear(animated)
+        let url = URL(string: "https: //www.google.com.br")!
+        webView.load(URLRequest(url: url)) */
+    }
+    
+    /* func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Carregou")
+    } */
+                
+    func viewDidAppear() {} // Vizualização Apareceu
+    
+    func viewDidDisappear() {}  // Vizualização Desapareceu
 
 }
 
+extension ViewController : WKNavigationDelegate {
+    
+}
