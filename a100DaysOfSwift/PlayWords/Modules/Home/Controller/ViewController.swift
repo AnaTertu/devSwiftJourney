38c9342be89a111d22f34d65c0228575ee6b4abb
AnaTertu <ana.tertu.2019@gmail.com>
2025-07-11 21:24:43 -0300

    add label clues

--- a100DaysOfSwift/PlayWords/Modules/Home/Controller/ViewController.swift
+++ a100DaysOfSwift/PlayWords/Modules/Home/Controller/ViewController.swift
@@ -18,10 +18,28 @@ class ViewController: UIViewController {
         scoreLabel.text = "Score: 0"
         view.addSubview(scoreLabel)
         
+        cluesLabel = UILabel()
+        cluesLabel.translatesAutoresizingMaskIntoConstraints = false
+        cluesLabel.font = UIFont.systemFont(ofSize: 24)
+        cluesLabel.textAlignment = .center
+        cluesLabel.text = "CLUES"
+        cluesLabel.numberOfLines = 0
+        cluesLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
+        view.addSubview(cluesLabel)
+        
         NSLayoutConstraint.activate([
             scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
             scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-        ])
+            
+            cluesLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+            cluesLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
+            cluesLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.6, constant: -100),
+            
+            ])
+        
+       
+        cluesLabel.backgroundColor = .lightGray
+        
     }
     
     override func viewDidLoad() {
