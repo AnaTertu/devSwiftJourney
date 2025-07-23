7d67e4a13d9ed53f45eaca8bcf8c4ef2cef89d85
AnaTertu <ana.tertu.2019@gmail.com>
2025-07-23 10:34:24 -0300

    Designing UICollectionView cells

--- a100DaysOfSwift/social_Media_ThirdProject/social_Media_ThirdProject/ViewController.swift
+++ a100DaysOfSwift/social_Media_ThirdProject/social_Media_ThirdProject/ViewController.swift
@@ -1,49 +1,29 @@
+//
+//  ViewController.swift
+//  social_Media_ThirdProject
+//
+//  Created by ana on 22/07/25.
+//
+
 import UIKit
 
-class ViewController: UITableViewController {
-    
-    var pictures = [String]()
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        title = "Storm Viewer"
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
-        
-        for item in items {
-            if item.hasPrefix("img") {
-                pictures.append(item)
-                
-                print(" encontrado item : ", item)
-            }
-        }
-        print(" encontrada ", pictures)
-        }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pictures.count
+
+        // Do any additional setup after loading the view.
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
-        cell.imageView?.image = UIImage(named: pictures[indexPath.row])
-        
-        return cell
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            
-            vc.selectedImage = pictures[indexPath.row]
-            
-            navigationController?.pushViewController(vc, animated: true)
-        }
-    }
+    */
+
 }
